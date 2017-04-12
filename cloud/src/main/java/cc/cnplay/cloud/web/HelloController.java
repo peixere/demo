ebc86b58fe2497c5b39cfdf2e21909d7e98a1bfc
peixere <peixere@qq.com>
2017-04-13 01:47:45 +0800

    init

---
+++ cloud/src/main/java/cc/cnplay/cloud/web/HelloController.java
@@ -0,0 +1,16 @@
+package cc.cnplay.cloud.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController
+{
+
+	@RequestMapping("/hello")
+	public String index()
+	{
+		return "Hello World";
+	}
+
+}
